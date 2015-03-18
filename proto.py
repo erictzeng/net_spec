53fab96a1fcab97835edaa1a09e3814b6cd4616f
Eric Tzeng <etzeng@eecs.berkeley.edu>
2015-03-18 13:29:44 -0700

    Better conversion from dictionary to messages, helper methods

--- proto.py
+++ proto.py
@@ -11,34 +11,33 @@ class ProtoObject(object):
     def __init__(self, msg_name, **kwargs):
         self._proto = getattr(caffe_pb2, msg_name)
         self._field_info = self._proto.DESCRIPTOR.fields_by_name
+        self._passthrough = True
         for key, value in kwargs.items():
-            if isinstance(value, dict):
-                message_desc = self._field_info[key].message_type
-                cls = self._message_type_to_protoclass(message_desc)
-                value = cls(message_desc.name, **value)
             setattr(self, key, value)
 
     def to_message(self):
-        message = self._proto()
-        for field, info in self._field_info.items():
-            if not hasattr(self, field):
-                if info.label == info.LABEL_REQUIRED:
-                    raise AttributeError('Net missing required field: {}'.format(field))
-                continue
-            val = getattr(self, field)
-            if info.label == info.LABEL_REPEATED:
-                if isinstance(val, basestring):
-                    raise AttributeError('Net field {} must be sequence, not string'.format(field))
-                if info.message_type:
-                    sub_msgs = [sub.to_message() for sub in val]
-                    getattr(message, field).extend(sub_msgs)
+        self._passthrough = False
+        try:
+            message = self._proto()
+            for field, info in self._field_info.items():
+                if not hasattr(self, field):
+                    if info.label == info.LABEL_REQUIRED:
+                        raise AttributeError('Net missing required field: {}'.format(field))
+                    continue
+                val = getattr(self, field)
+                if info.label == info.LABEL_REPEATED:
+                    if info.message_type:
+                        sub_msgs = [sub.to_message() for sub in val]
+                        getattr(message, field).extend(sub_msgs)
+                    else:
+                        getattr(message, field).extend(val)
                 else:
-                    getattr(message, field).extend(val)
-            else:
-                if info.message_type:
-                    getattr(message, field).CopyFrom(val.to_message())
-                else:
-                    setattr(message, field, val)
+                    if info.message_type:
+                        getattr(message, field).CopyFrom(val.to_message())
+                    else:
+                        setattr(message, field, val)
+        finally:
+            self._passthrough = True
         return message
 
     @staticmethod
@@ -48,35 +47,62 @@ class ProtoObject(object):
         elif message_type.name == 'LayerParameter':
             return ProtoLayer
         else:
-            return ProtoParam
+            return getattr(ProtoParam, message_type.name)
 
-    @classmethod
-    def from_message(cls, message):
-        if message.DESCRIPTOR.name in ('NetParameter', 'LayerParameter'):
-            protoobj = cls()
-        else:
-            protoobj = cls(message.DESCRIPTOR.name)
+    @staticmethod
+    def from_message(message):
+        cls = ProtoObject._message_type_to_protoclass(message.DESCRIPTOR)
+        protoobj = cls()
         protoobj._message = message
         msg_fields = [field[0].name for field in message.ListFields()]
         for field, info in protoobj._field_info.items():
             if field not in msg_fields:
                 continue
             val = getattr(message, field)
+            if info.label == info.LABEL_REPEATED:
+                val = list(val)
             if info.message_type:
                 if info.label == info.LABEL_REPEATED:
                     protocls = cls._message_type_to_protoclass(info.message_type)
-                    setattr(protoobj, field, [protocls.from_message(param_msg) for param_msg in val])
+                    setattr(protoobj, field, [ProtoObject.from_message(param_msg) for param_msg in val])
                 else:
-                    setattr(protoobj, field, ProtoParam.from_message(val))
+                    setattr(protoobj, field, ProtoObject.from_message(val))
             else:
                 setattr(protoobj, field, val)
         return protoobj
 
     def __setattr__(self, name, value):
-        if name.startswith('_') or name in self._field_info:
+        if name.startswith('_'):
+            object.__setattr__(self, name, value)
+        elif name in self._field_info:
+            info = self._field_info[name]
+            # allow single value assignment to repeated fields
+            if info.label == info.LABEL_REPEATED and not isinstance(value, (list, tuple)):
+                value = [value]
+            # convert dictionaries to ProtoObjects if necessary
+            message_desc = self._field_info[name].message_type
+            if message_desc:
+                cls = self._message_type_to_protoclass(message_desc)
+                if info.label == info.LABEL_REPEATED:
+                    new_value = []
+                    for el in value:
+                        if isinstance(el, dict):
+                            new_value.append(cls(**el))
+                        else:
+                            new_value.append(el)
+                    value = new_value
+                elif isinstance(value, dict):
+                    value = cls(message_desc.name, **value)
             object.__setattr__(self, name, value)
         else:
             raise AttributeError('Invalid field {}'.format(name))
+
+    def __getattr__(self, name):
+        if self._passthrough and name in self._field_info:
+            default = self._field_info[name].default_value
+            if default is not None:
+                return default
+        raise AttributeError('Invalid field {}'.format(name))
 
 
 class ProtoNet(ProtoObject):
@@ -117,6 +143,14 @@ class ProtoNet(ProtoObject):
                 result[layer.include[0].phase] = layer
             return result
 
+    def __delitem__(self, name):
+        indices = []
+        for i, layer in enumerate(self.layer):
+            if layer.name == name:
+                indices.append(i)
+        for index in indices[::-1]:
+            del self.layer[index]
+
     def write_prototxt(self, outpath):
         with open(outpath, 'w') as f:
             f.write(protobuf.text_format.MessageToString(self.to_message()))
@@ -136,6 +170,10 @@ class ProtoLayer(ProtoObject):
 
     def __repr__(self):
         return '<ProtoLayer {} {}>'.format(self.type, self.name)
+
+    def set_phase(self, phase):
+        self.include = [ProtoParam.NetStateRule(phase=phase)]
+        return self
 
 
 class FakeLayerClass(object):
